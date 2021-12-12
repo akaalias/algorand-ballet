660e04dcb86ac8e6c1376dee9d025c0545f9fd21
Alexis Rondeau <alexis.rondeau@gmail.com>
2021-12-12 12:22:47 +0100

    Renames deploy.sh to ship.sh

--- ship.sh
+++ ship.sh
@@ -6,14 +6,14 @@ set -e
 # build
 npm run build
 
-echo -n "Commit message: "
+echo -n "My commit message: "
 read MESSAGE
 
 echo $MESSAGE
 
 git add -A
 git commit -m $MESSAGE
-#git push origin main
+git push origin main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
